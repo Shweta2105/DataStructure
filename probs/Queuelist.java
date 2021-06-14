38a8b94fa88bd62370fb4240acf591d26b7005d7
Shweta2105 <sghadi21@gmail.com>
2021-06-14 07:52:06 +0530

    element appended to queue

---
+++ probs/Queuelist.java
@@ -0,0 +1,23 @@
+package DataStructure.probs;
+
+public class Queuelist {
+	MyLinkedList mylist = new MyLinkedList();
+
+	public Queuelist() {
+		this.mylist=mylist;
+		
+	}
+
+	public void append(INode mynode) {
+		mylist.append(mynode);
+		
+	}
+
+	public void printMyNodes() {
+		mylist.printMyNodes();
+		
+	}
+	
+	
+
+}
---
+++ probs/Queuemain.java
@@ -0,0 +1,22 @@
+package DataStructure.probs;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class Queuemain {
+	@Test
+	public void insertValueInQueue() {
+		System.out.println("Appending element to bottom");
+		MyNode<Integer> MyFirstnode = new MyNode<>( 56);
+		MyNode<Integer> MySecondnode = new MyNode<>( 30);
+		MyNode<Integer> MyThirdnode = new MyNode<>( 70);
+		Queuelist myQueue = new Queuelist();
+		myQueue.append(MyFirstnode); //calling append function
+		myQueue.append(MySecondnode);
+		myQueue.append(MyThirdnode);
+		myQueue.printMyNodes();
+		Assert.assertNotNull(myQueue);
+	}
+
+}
