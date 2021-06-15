ade214acb9ecf52f48622b7c858be756965dbcb1
Shweta2105 <sghadi21@gmail.com>
2021-06-15 07:49:39 +0530

    Frequency of word paranoid from string calculated

--- probs/MyHashMapTest.java
+++ probs/MyHashMapTest.java
@@ -25,4 +25,24 @@ public class MyHashMapTest {
 		Assert.assertEquals(2, frequency);
 		
 	}
+	
+	@Test
+	public void givenA_Paranoid_Sentence_ReturnWordFrequency() {
+		String sentence ="Paranoid are not paranoid because they are"
+				+ " paranoid but because thy keep putting themselves"
+				+ "deliberately into paranoid avoidable situations";
+		myHashMap<String, Integer> myHash = new myHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for(String word : words) {
+			Integer value = myHash.get(word);
+			if(value == null) value = 1;
+			else value = value + 1;
+			myHash.add(word,value);
+			
+		}
+		int frequency = myHash.get("paranoid");
+		System.out.println(myHash);
+		Assert.assertEquals(2, frequency);
+		
+	}
 }
