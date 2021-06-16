65cc8e2e864c0a0599811397e4e983ccab114597
Shweta2105 <sghadi21@gmail.com>
2021-06-16 22:52:11 +0530

    Added element to list

---
+++ probs/BinarySearchTree.java
@@ -0,0 +1,98 @@
+package DataStructure.probs;
+
+public class BinarySearchTree {  
+public static class Node{  
+        int data;  
+        Node left;  
+        Node right;  
+  
+        public Node(int data){  
+             
+            this.data = data;  
+            this.left = null;  
+            this.right = null;  
+        }  
+      }  
+  
+       
+      public Node root;  
+  
+      public BinarySearchTree(){  
+          root = null;  
+      }  
+  
+        
+      public void insert(int data) {  
+           
+          Node newNode = new Node(data);  
+  
+          
+          if(root == null){  
+              root = newNode;  
+              return;  
+            }  
+          else {  
+               
+              Node current = root, parent = null;  
+  
+              while(true) {  
+                   
+                  parent = current;  
+  
+                   
+                  if(data < current.data) {  
+                      current = current.left;  
+                      if(current == null) {  
+                          parent.left = newNode;  
+                          return;  
+                      }  
+                  }  
+                    
+                  else {  
+                      current = current.right;  
+                      if(current == null) {  
+                          parent.right = newNode;  
+                          return;  
+                      }  
+                  }  
+              }  
+          }  
+      }  
+  
+      
+     
+      
+      public void inorderTraversal(Node node) {  
+  
+        
+          if(root == null){  
+              System.out.println("Tree is empty");  
+              return;  
+           }  
+          else {  
+  
+              if(node.left!= null)  
+                  inorderTraversal(node.left);  
+              System.out.print(node.data + " ");  
+              if(node.right!= null)  
+                  inorderTraversal(node.right);  
+  
+          }  
+      }  
+  
+      public static void main(String[] args) {  
+  
+          BinarySearchTree bt = new BinarySearchTree();  
+          //Add nodes to the binary tree  
+          bt.insert(56);  
+          bt.insert(30);  
+          bt.insert(70);  
+          
+  
+          System.out.println("Binary search tree after insertion:");  
+           
+          bt.inorderTraversal(bt.root);  
+  
+          
+      }  
+}
